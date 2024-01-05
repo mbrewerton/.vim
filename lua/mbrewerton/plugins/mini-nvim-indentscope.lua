c902791c70146b3a12e6743d35d6de4a4cac77c7
Matt Brewerton <matt@mattbrewerton.co.uk>
2024-01-05 11:27:40 +0000

    Add mini plugins

---
+++ lua/mbrewerton/plugins/mini-nvim-indentscope.lua
@@ -0,0 +1,11 @@
+return {
+    'echasnovski/mini.indentscope',
+    version = '*',
+    config = function ()
+        require('mini.indentscope').setup(
+            {
+                symbol = '|'
+            }
+        )
+    end
+}
