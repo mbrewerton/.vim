442802c7868c8bd6a85c4ba0bac02f704b11fe2e
Matt Brewerton <matt@mattbrewerton.co.uk>
2024-02-28 11:20:07 +0000

    Implement LSP setup with lsp-zero

---
+++ lua/mbrewerton/plugins/lsp-zero.lua
@@ -0,0 +1,20 @@
+return {
+    'VonHeikemen/lsp-zero.nvim',
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+    },
+    config = function()
+        local lsp_zero = require('lsp-zero')
+
+        lsp_zero.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp_zero.default_keymaps({ buffer = bufnr })
+        end)
+
+        lsp_zero.setup()
+    end
+}
